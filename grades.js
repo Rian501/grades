dc9296cfb21ba82b37acc90cb2bd0249381d3231
Rian501 <eclaire.lemmon@gmail.com>
2017-06-07 15:27:23 -0500

    Sort grades into groups and now can output all requirements

--- grades.js
+++ grades.js
@@ -4,33 +4,17 @@ let sortedScores = scores.sort(function(first, second) {
 	return first - second;
 });
 
-let smallFirst = sortedScores.reverse();
 
-console.log("sorted scores", sortedScores);
+console.log("sorted scores small to big", sortedScores);
 
+sortedScores = sortedScores.reverse();
 
-
+console.log("sorted scores big to small", sortedScores);
 var Fgroup = [];
 var Dgroup = [];
 var Cgroup = [];
 var Bgroup = [];
 var Agroup = [];
-
-
-// How many of each grade? (A-F)  slice? pop? push?  DONT use pop, it removes from original list... slice?
-for (i=0; i<scores.length; i++)
-  if (sortedScores[scores.length-i] <= 60)  {
-  	Fgroup += sortedScores.pop();
-    } else if (sortedScores[i] <=70) {
-  	Dgroup += sortedScores.pop();
-  } else if (sortedScores[i] <=80) {
-  	Cgroup += sortedScores.pop();
-  }
-
-console.log("Fgroup", Fgroup);
-
-console.log("Dgroup", Dgroup);
-
 
 
 // What is the lowest grade?
@@ -39,5 +23,41 @@ lowestGrade = sortedScores.pop();
 
 console.log("The lowest grade is", lowestGrade);
 
+sortedScores.push(lowestGrade);
+
+// console.log("sorted scores after lowest grade popped in and back out?", sortedScores);
+
 
 // What is the highest grade?
+
+highestGrade = sortedScores.shift();
+
+console.log("The highest grade is", highestGrade);
+
+sortedScores.unshift(highestGrade);
+
+// console.log("sorted scores after highest grade popped in and back out?", sortedScores);
+
+
+// How many of each grade? (A-F)  
+for (i=0; i<scores.length; i++) {
+	if (sortedScores[i] >= 91) {
+		Agroup.unshift(sortedScores[i]) 		
+	} else if (sortedScores[i] >= 81) {
+		Bgroup.unshift(sortedScores[i]) 		
+	} else if (sortedScores[i] >= 71) {
+		Cgroup.unshift(sortedScores[i]) 		
+	} else if (sortedScores[i] >= 61) {
+		Dgroup.unshift(sortedScores[i]) 		
+	} else if (sortedScores[i] >= 50) {
+		Fgroup.unshift(sortedScores[i]) 		
+	}
+
+ }
+
+console.log("How many As?", Agroup.length);
+
+console.log("How many Bs?", Bgroup.length);
+console.log("How many Cs?", Cgroup.length);
+console.log("How many Ds?", Dgroup.length);
+console.log("How many Fs?", Fgroup.length);
